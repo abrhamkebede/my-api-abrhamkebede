e9f10325df3cf407c9ab4d084bf04a4678b60398
abrham <abrhamkebede200@gmail.com>
2022-04-30 10:46:48 -0700

    Update week1, read and write methods.

--- src/tests/console/week01/ReverseWordsTest.java
+++ src/tests/console/week01/ReverseWordsTest.java
@@ -3,12 +3,14 @@ package tests.console.week01;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ReverseWordsTest {
 
-    public static void intro(){
+    public static void intro() {
         System.out.println();
         System.out.println();
         System.out.println("========================================================");
@@ -25,15 +27,39 @@ public class ReverseWordsTest {
         System.out.println();
     }
 
-    public static void read(String inputLocation, List<String> list) throws FileNotFoundException{
+    public static void read(String inputLocation, List<String> list) throws FileNotFoundException {
         //TODO: READ IN DATA FROM INPUT FILE LOCATION
+
+        File file = new File(inputLocation);
+        Scanner input = new Scanner(file);
+
         //TODO: REVERSE LINES
-        //TODO: REVERSE WORDS
-    }
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            int x = line.length();
+            if (x > 0) {
+                String reverseString = "";
+                for (int i = x - 1; i >= 0; i--) {
+                    reverseString += line.charAt(i);
+                }
+
+                list.add(reverseString);
+            }
+        }
+            input.close();
+        }
 
 
-    public static void write(String outputLocation, List<String> list) throws FileNotFoundException{
+
+
+    public static void write(String outputLocation, List<String> list) throws FileNotFoundException {
         //TODO : WRITE TO OUTPUT FILE LOCATION
+        PrintStream output = new PrintStream(outputLocation);
+        for (String y : list) {
+            output.println(y);
+        }
+        output.close();
+
     }
 
 
@@ -42,15 +68,13 @@ public class ReverseWordsTest {
                                       + File.separator + "words.txt";
         String outputLocation   = "." + File.separator + "results"
                                       + File.separator + "reverse_words.txt";
-        File file               = new File(inputLocation);
-        Scanner input           = new Scanner(file);
-        PrintStream output      = new PrintStream(outputLocation);
 
         //TODO: update program to use an Array List
 
-        // TODO: process items in file
+        List <String> list = new ArrayList<>();
 
-        input.close();
-        output.close();
+        read(inputLocation,list);
+        write(outputLocation,list);
+
     }
 }
