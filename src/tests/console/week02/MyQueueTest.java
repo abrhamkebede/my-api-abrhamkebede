3e5f5c9208890238884d61ca58b6e342afcf4829
abrham <abrhamkebede200@gmail.com>
2022-05-10 19:01:54 -0700

    MyQueue JUnit  Test  and MyQueue Test week02

--- src/tests/console/week02/MyQueueTest.java
+++ src/tests/console/week02/MyQueueTest.java
@@ -123,13 +123,108 @@ public class MyQueueTest {
     public static void myVersionTest(){
         System.out.println();
         System.out.println("==========  M Y   Q U E U E  ==========");
+        MyQueue<String> queue = new MyQueue<>() ;
+        MyQueue<String> queue1 = new MyQueue<>();
+        MyQueue<String> queue2 = new MyQueue<>();
+        queue.add("Red");
+        queue.add("Orange");
+        queue.add("Yellow");
+        queue.add("Green");
+        queue.add("Blue");
+        queue.add("Indigo");
+        queue.add("Violet");
+        System.out.println(queue);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //test remove
+        System.out.println("************Test remove/ size /isEmpty**************");
+        System.out.println();
+        System.out.println("----------original-----------");
+        System.out.println("size: " + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println();
+        System.out.println("----------1st remove-----------");
+        System.out.println("Remove element: " + queue.remove());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------2nd remove-----------");
+        System.out.println("Remove element: " + queue.remove());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------3rd remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------4th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------5th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------6th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------7th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------isEmpty() peek -----------");
+        System.out.println("Remove element: " + queue.peek());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+
+
+        //test peek
+        System.out.println("************Test peek**************");
+        queue1.add("Red");
+        queue1.add("Orange");
+        queue1.add("Yellow");
+        System.out.println(queue1);
+        System.out.println(queue1.peek());
+        System.out.println();
+
+
+        //test add
+        System.out.println("************Test add and print front and back **************");
+        queue2.add("Red");
+        queue2.add("Orange");
+        queue2.add("Yellow");
+        System.out.print("Front ");
+        System.out.print(queue2);
+        System.out.println("Back");
+        System.out.println();
+        System.out.println();
+
+
     }
 
 
     public static void main(String[] args) {
         intro();
-        libraryVersionTest();
-//        myVersionTest();
+      //  libraryVersionTest();
+        myVersionTest();
     }
 
 }
--- src/utils/MyQueue.java
+++ src/utils/MyQueue.java
@@ -1,11 +1,124 @@
 package utils;
 
+import java.util.NoSuchElementException;
+
 /*****************************************************************
  * This class MyQueue implements a FIFO data structure using a
  * Singly Linked List (SLL).
  *****************************************************************/
 
-public class MyQueue {
+public class MyQueue<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
 
-    //TODO : Complete Body with Data Fields, Methods and Classes
+    public MyQueue() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public boolean add(E data) {
+        int preSize = this.size;
+        Node<E> node = new Node<>(data);
+        if (this.head == null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+           // this.tail.setNext(node);
+            //this.tail = node;
+            append(data);
+        }
+        this.size++;
+        return this.size != preSize;
+    }
+
+   private void append(E data) {
+        Node<E> node = new Node<>(data);
+        if (this.head == null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+    }
+
+    public E remove() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("NoSuchElementException");
+        }
+        this.size--;
+        E data = this.head.getData();
+        this.head = this.head.getNext();
+        return data;
+
+    }
+
+    public void detach() {
+        if (this.head == null) {
+            throw new NoSuchElementException("NoSuchElementException");
+        }
+        this.head = this.head.getNext();
+    }
+
+    public String toString() {
+        if(isEmpty()) return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<E> tmp = this.head;
+        for (tmp = this.head; tmp!= null; tmp = tmp.getNext()) {
+
+            if(tmp.next !=null) {
+                sb.append(tmp.getData()).append(", ");
+            }
+            else{
+                sb.append(tmp.getData()).append("]");
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+    public E peek() {
+        if (this.head == null) {
+            return null;
+        }
+        return this.head.getData();
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
 }
+
